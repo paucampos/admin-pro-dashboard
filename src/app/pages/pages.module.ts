228f4380e3ee58ac0825451bcae850cfb5ca91ad
Paula Katerinne Campos Campos <pau.campos89@gmail.com>
2019-04-06 19:34:39 -0300

    Crear modulos y rutas personalizadas2

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+// modulos
+import { SharedModule } from "../shared/shared.module";
+
+// componentes
+import { PagesComponent } from "./pages.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+// Rutas
+import { PAGES_ROUTES } from "./pages.routes";
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule {}
