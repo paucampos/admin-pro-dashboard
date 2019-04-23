dab2361c63ee68737b0850b00731f608f54345c2
Paula Katerinne Campos Campos <pau.campos89@gmail.com>
2019-04-23 18:14:59 -0400

    Cambiar el check de tema

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -15,8 +15,20 @@ export class AccountSettingsComponent implements OnInit {
   ngOnInit() {
   }
 
-  cambiarColor(tema: string) {
+  cambiarColor(tema: string, link: any) {
+    this.aplicarCheck(link);
     let urlTema = `assets/css/colors/${tema}.css`;
     this._document.getElementById('theme').setAttribute('href', urlTema);
+  }
+
+  aplicarCheck(link: any){
+    let selectors: any = document.getElementsByClassName('selector');
+
+    for(let ref of selectors) {
+      // vanilla javascript
+      ref.classList.remove('working');
+    }
+    // vanilla javascript
+    link.classList.add('working');
   }
 }
